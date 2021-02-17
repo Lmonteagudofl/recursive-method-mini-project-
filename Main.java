c5ed1693c61b46b58de16424b8873b8f94c7a4fc
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2021-02-17 16:39:03 +0000

    i added an if else statement

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String[] args) {
     minusOne(3);
     System.out.println("We are going to be racing cars today!! On My Mark!");
+    System.out.println("We are going to be racing cars today!! O");
   }
 
   public static void minusOne(int n) {
