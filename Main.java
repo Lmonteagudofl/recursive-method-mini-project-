3fcd12d7842a71e0c24d4e692cc3af63947e8baf
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2021-02-17 16:38:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    minusOne(3);
+    System.out.println("We are going to be racing cars today!! On My Mark!");
+  }
+
+  public static void minusOne(int n) {
+    if (n == 0) {
+      System.out.println("GO!");
+    } else {
+      System.out.println(n);
+      n--;
+      minusOne(n);
+      }
+    }
+  }
