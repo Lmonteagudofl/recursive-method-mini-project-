41c30c0fe9de9ef5c9b94b9ca73e081e0b4611fe
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2021-02-19 15:41:28 +0000

    i added scanner

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
     System.out.println("We are going to be racing cars!!");
+    System.out.println("Are you ready? reply Yes or no");
+    String name =  scan.next();
+
     System.out.println("On My Mark!!");
     System.out.println("Get set!!");
 
+    
     int result = minusOne(3);
-    System.out.println(result);
+
+    //System.out.println(result);
     System.out.println("GO!");
+
   }
 
   public static int minusOne(int n) {
-    if (n == 1) {
+    if (n == 0) {
+
     } else {
       System.out.println(n);
       n--;
