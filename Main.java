9f60329c72c3d29382f0391cf245426cf05983b5
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2021-02-19 14:43:27 +0000

    i added a return statement

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    minusOne(3);
-    System.out.println("We are going to be racing cars today!! On My Mark!");
-    System.out.println("We are going ");
+    System.out.println("We are going to be racing cars!!");
+    System.out.println("On My Mark!!");
+    System.out.println("Get set!!");
+
+    int result = minusOne(3);
+    System.out.println(result);
+    System.out.println("GO!");
   }
 
-  public static void minusOne(int n) {
-    if (n == 0) {
-      System.out.println("GO!");
+  public static int minusOne(int n) {
+    if (n == 1) {
     } else {
       System.out.println(n);
       n--;
       minusOne(n);
-      }
     }
+    return 1; 
   }
+}
